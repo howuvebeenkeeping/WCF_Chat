05ad89877a154f64c4c940d849dc84d3596a3b8d
howuvebeenkeeping <rigabdullin@yandex.ru>
2020-08-06 16:24:33 +0500

    Create server user

---
+++ WCF_Chat/ServerUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_Chat
+{
+    public class ServerUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public OperationContext OperationContext { get; set; }
+
+    }
+}
