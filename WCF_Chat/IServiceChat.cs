7587239ba188a5c4e273c586fb2b7fa99d733f0e
howuvebeenkeeping <rigabdullin@yandex.ru>
2020-08-06 16:09:14 +0500

    Create service lib

---
+++ WCF_Chat/IServiceChat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Chat
+{
+
+    [ServiceContract(CallbackContract = typeof(IServerChatCallback))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        void Connect();
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string message);
+    }
+
+    public interface IServerChatCallback
+    {
+        [OperationContract]
+        void MessageCallback(string message);
+    }
+}
---
+++ WCF_Chat/ServiceChat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Chat
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceChat : IServiceChat
+    {
+        public void Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
