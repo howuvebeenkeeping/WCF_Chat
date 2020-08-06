11d7bad964233e737df0e1375da2483ecde41c9b
howuvebeenkeeping <rigabdullin@yandex.ru>
2020-08-06 15:21:44 +0500

    Create service library

---
+++ ServiceLibrary/IServiceChat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceLibrary
+{
+    [ServiceContract(CallbackContract = typeof(IServerChatCallback))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        void Connect();
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string message);
+    }
+
+    public interface IServerChatCallback
+    {
+        [OperationContract]
+        void MessageCallback(string message);
+    }
+}
